aa479da75cabee1cd337d8b027c3b3cb8dcc515c
Boris Tumbev <b.tumbev@code-nest.com>
2019-11-14 17:27:42 +0200

    fixed logic for dynamic form in sruvey

--- code/uni_system/uni_system_fe/src/components/blocks/survey/SurveyForm.js
+++ code/uni_system/uni_system_fe/src/components/blocks/survey/SurveyForm.js
@@ -28,6 +28,16 @@ const { Option } = Select;
 
 export class SurveyForm extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            questions_count: -1,
+            answer_count: -1,
+            answer_arr: {},
+        }
+    }
+
     componentDidMount(){
         this.props.getMajors()
 
@@ -49,55 +59,45 @@ export class SurveyForm extends Component {
   };
 
   add = () => {
-    const { form } = this.props;
-    // can use data-binding to get
-    const keys = form.getFieldValue("newkeys");
-    const nextKeys = keys.concat(uuid);
-    uuid++;
-    // can use data-binding to set
-    // important! notify form to detect changes
-    form.setFieldsValue({
-      newkeys: nextKeys
-    });
+
+    let q_count = this.state.questions_count + 1;
+    let q = 'q' + q_count;
+    this.setState(prevState => {
+       return {
+                questions_count: prevState.questions_count+1,
+                answer_arr: {...prevState.answer_arr, [q]: []}
+        }
+    })
   };
 
-  remove1 = (k, l) => {
-    const { form } = this.props;
-    // can use data-binding to get
-    const keys = form.getFieldValue("answerkey" + k);
-    let newkeys = [];
-    // We need at least one passenger
-    if (keys) {
-      newkeys = keys;
-    } else {
-      newkeys = [];
-    }
-    if (newkeys.length === 1) {
-      //return;
-    }
-    // can use data-binding to set
-    form.setFieldsValue({
-      ["answerkey" + k]: newkeys.filter(key => key !== l)
-    });
+  remove1 = (a, i) => {
+    console.log('remove answe');
+    console.log(i);
+    console.log(a);
+        var q = 'q'+i
+        var a_count = this.state.answer_arr[q].filter(item => item !== a)
+        this.setState(prevState => {
+           return {
+//                answer_count: prevState.answer_count - 1,
+                answer_arr: {...prevState.answer_arr, [q]: a_count}}
+        })
   };
 
   add1 = index => {
-    const { form } = this.props;
-    // can use data-binding to get
-    const keys = form.getFieldValue("answerkey" + index);
-    let newkeys = [];
-    if (keys) {
-      newkeys = keys;
-    } else {
-      newkeys = [];
-    }
-    const nextKeys = newkeys.concat(uuid1);
-    uuid1++;
-    // can use data-binding to set
-    // important! notify form to detect changes
-    form.setFieldsValue({
-      ["answerkey" + index]: nextKeys
-    });
+    console.log('add answ');
+    console.log(index);
+
+    var q = 'q'+index
+    var a_count = this.state.answer_count + 1
+    var answ = 'a' + a_count
+    this.state.answer_arr[q].push(answ)
+    var a_arr = this.state.answer_arr[q]
+    this.setState(prevState => {
+       return {
+                answer_count: prevState.answer_count + 1,
+                answer_arr: {...prevState.answer_arr, [q]: a_arr}
+       }
+    })
   };
 
   handleSubmit = e => {
@@ -112,6 +112,98 @@ export class SurveyForm extends Component {
         return this.props.majors.map(function (e, index){
           return <Option key={e.id} value={e.id}>{e.name}</Option>
         })
+    }
+
+    questionsRender(){
+        console.log('vlena bate maiko');
+        console.log(this.state);
+        const { getFieldDecorator, getFieldValue } = this.props.form;
+        let that = this;
+        var i, k;
+        var answers = []
+//        for(i=0; i<=this.state.questions_count; i++){
+//            console.log('i');
+//            console.log(i);
+//              questions.push(
+
+
+        return Object.keys(this.state.answer_arr).map(function(e, i){
+                console.log('aa');
+                console.log(e);
+                console.log(i);
+                console.log(that.state.answer_arr[e]);
+                answers = []
+                that.state.answer_arr[e].map(function(a, a_i){
+                    answers.push(
+                        <div key={a_i} style={{marginLeft:"65px"}}>
+                          <Form.Item label="answer">
+                            {getFieldDecorator('answer' + a + e, {
+                                rules: [{ required: true,
+                                          message: 'Please input answer!'
+                                }],
+                            })(
+                                <Input/>,
+                            )}
+                        </Form.Item>
+                        <Icon
+                            className="dynamic-delete-button"
+                            type="minus-circle-o"
+                            onClick={() => that.remove1(a, i)}
+
+                        />
+                        </div>
+                    )
+
+                })
+
+//                for(k=0; k<that.state.answer_count[e]; k++){
+//                    answers.push(
+//                    <div key={k} style={{marginLeft:"65px"}}>
+//                      <Form.Item label="answer">
+//                        {getFieldDecorator('answer' + k + e, {
+//                            rules: [{ required: true,
+//                                      message: 'Please input answer!'
+//                            }],
+//                        })(
+//                            <Input/>,
+//                        )}
+//                    </Form.Item>
+//                    <Icon
+//                        className="dynamic-delete-button"
+//                        type="minus-circle-o"
+//                        onClick={() => that.remove1(i)}
+//
+//                    />
+//                    </div>
+//                    )
+//                }
+
+            return(
+                  <div key={i} style={{marginLeft:"20px"}}>
+                  <Form.Item label="question">
+                    {getFieldDecorator('question' + i, {
+                        rules: [{ required: true,
+                                  message: 'Please input question!'
+                        }],
+                    })(
+                        <Input/>,
+                    )}
+                </Form.Item>
+                <Icon
+                    className="dynamic-delete-button"
+                    type="minus-circle-o"
+                    onClick={() => this.remove(i)}
+                />
+                {answers}
+                <div className="VallAs" onClick={() => that.add1(i)}>
+                  <span>+ Add Answer</span>
+                </div>
+                </div>
+            )
+       })
+//            )
+//        }
+        return questions
     }
 
   render() {
@@ -132,84 +224,10 @@ export class SurveyForm extends Component {
         sm: { span: 20, offset: 4 }
       }
     };
-    var newkeys = [];
-    var answekeys = [];
-    var correctAnswer = [];
-    getFieldDecorator("newkeys", { initialValue: [] });
-    newkeys = getFieldValue("newkeys");
-    for (var i = 0; i < newkeys.length; i++) {
-      getFieldDecorator("answerkey" + i, { initialValue: [] });
 
-    }
-    const formItems = newkeys.map((k, index) => {
-      answekeys = getFieldValue("answerkey" + k);
-      correctAnswer = getFieldValue("rightanswer" + k);
-      if (
-        answekeys == undefined ||
-        answekeys == null ||
-        answekeys.length == 0
-      ) {
-        answekeys = [];
-      }
-      return (
-        <div>
-          <FormItem className="Quest">
-            {getFieldDecorator("question" + k, {
-              validate: [
-                {
-                  trigger: ["onBlur"],
-                  rules: [
-                    {
-                      required: true,
-                      message: "Please enter your Question!"
-                    }
-                  ]
-                }
-              ]
-            })(<Input type="" placeholder="Enter Question" />)}
-            {newkeys.length > 1 ? (
-              <Icon
-                className="dynamic-delete-button"
-                type="minus-circle-o"
-                onClick={() => this.remove(k)}
-              />
-            ) : null}
-          </FormItem>
-          <ol className="AswField">
-            {answekeys &&
-              answekeys.length > 0 &&
-              answekeys.map((l, index1) => {
-                return (
-                    <FormItem>
-                      {getFieldDecorator("answer" + l, {
-                        validate: [
-                          {
-                            trigger: ["onBlur"],
-                            rules: [
-                              {
-                                required: true,
-                                message: "Please enter your Answer"
-                              }
-                            ]
-                          }
-                        ]
-                      })(<Input type="" placeholder=" Enter Answer" />)}
-                      <span
-                        className="TickCsl"
-                        onClick={() => this.remove1(k, l)}
-                      >
-                        <Icon type="delete" />
-                      </span>
-                    </FormItem>
-                );
-              })}
-            <div className="VallAs" onClick={() => this.add1(k)}>
-              <span>+ Add Answer</span>
-            </div>
-          </ol>
-        </div>
-      );
-    });
+    console.log('maikataaaa');
+    console.log(this.state.questions_count);
+
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Item {...formItemLayout} label="Title">
@@ -230,7 +248,7 @@ export class SurveyForm extends Component {
             </Select>,
             )}
         </Form.Item>
-        {formItems}
+        {this.questionsRender()}
         <FormItem {...formItemLayoutWithOutLabel}>
           <Button type="dashed" onClick={this.add} style={{ width: "60%" }}>
             <Icon type="plus" /> Add field
