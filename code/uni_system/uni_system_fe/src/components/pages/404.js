bc3f44d053867b1e75a7af599e2b0079eb4f2ebc
Boris Tumbev <b.tumbev@code-nest.com>
2019-11-27 17:18:37 +0200

    added 404 page

---
+++ code/uni_system/uni_system_fe/src/components/pages/404.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Result, Button } from 'antd';
+
+
+export class Error404 extends Component {
+
+
+    backHome(){
+        window.location.href = "/";
+    }
+
+    render() {
+        return (
+            <>
+                <Result
+                    status="404"
+                    title="404"
+                    subTitle="Sorry, the page you visited does not exist."
+                    extra={<Button onClick={(e) => {this.backHome()}} type="primary">Back Home</Button>}
+                />,
+            </>
+        );
+
+    }
+}
+
+
+export default (Error404);
--- code/uni_system/uni_system_fe/src/routes.js
+++ code/uni_system/uni_system_fe/src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Login from "./components/pages/Login";
 import Home from "./components/pages/Home";
@@ -8,17 +8,28 @@ import Teachers from "./components/pages/Teachers";
 import Profile from "./components/pages/Profile";
 import Survey from "./components/pages/Survey";
 import SurveyResolve from "./components/blocks/survey/SurveyResolve";
+import Error404 from "./components/pages/404";
+
+
+const NoMatchPage = () => {
+    return (
+        <h3>404 - Not found</h3>
+    );
+};
 
 const BaseRouter = () => (
-  <div>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/students" component={Students} />
-    <Route exact path="/teachers" component={Teachers} />
-    <Route exact path="/profile" component={Profile} />
-    <Route exact path="/survey" component={Survey} />
-    <Route exact path="/survey/:id" component={SurveyResolve} />
-  </div>
+    <div>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/students" component={Students} />
+            <Route exact path="/teachers" component={Teachers} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/survey" component={Survey} />
+            <Route exact path="/survey/:id" component={SurveyResolve} />
+            <Route component={Error404} />
+        </Switch>
+    </div>
 );
 
 export default BaseRouter;
