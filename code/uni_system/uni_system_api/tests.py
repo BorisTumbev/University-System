35c823b27c1794050bf5eaeb8b44ba11765b0666
Boris Tumbev <b.tumbev@code-nest.com>
2020-07-08 20:14:43 +0300

    added one test and desc updated

--- code/uni_system/uni_system_api/tests.py
+++ code/uni_system/uni_system_api/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+class StudentsTest(TestCase):
+
+    def test_students(self):
+
+        response_get_students = self.client.get('/api/students', follow=True)
+        self.assertEqual(response_get_students.status_code, 200)
+        self.assertEqual(response_get_students.json(), list())
+
+        student_data_post = {
+            "student_profile":{
+                    "qualification_type":"MD",
+                    "group":3,
+                    "faculty":"FEA",
+                    "semester":1
+                 },
+            "password":"admin",
+            "is_superuser":False,
+            "username":"Lupe.Bruen",
+            "first_name":"Eva",
+            "last_name":"Hessel",
+            "surname":"Keanu_Pfeffer",
+            "email":"your.email+faker68612@gmail.com"
+        }
+
+        response_post_students = self.client.post('/api/students', student_data_post, follow=True)
+
+        self.assertEqual(response_post_students.status_code, 200)
+        self.assertEqual(response_post_students.json(), dict())
